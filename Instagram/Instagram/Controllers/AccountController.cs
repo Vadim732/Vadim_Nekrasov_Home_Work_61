e03010cc3f895af4c8432f644cb2ca364d740f7c
Kirill <kirillsaydanov@gmail.com>
2024-11-13 21:16:08 +0600

    Правки в вёрстке

--- Instagram/Instagram/Controllers/AccountController.cs
+++ Instagram/Instagram/Controllers/AccountController.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Instagram.Models;
+﻿using Instagram.Models;
 using Instagram.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
--- Instagram/Instagram/Controllers/PublicationController.cs
+++ Instagram/Instagram/Controllers/PublicationController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 using System.Security.Claims;
 
 namespace Instagram.Controllers;
